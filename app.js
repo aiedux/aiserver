2a1e365900022aa40bca8ee73b674636bf40e5da
stefian <v_ianta@yahoo.com>
2020-02-02 09:58:02 -0400

    added required variables

---
+++ app.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let path = require('path');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let redis = require('redis');
+
