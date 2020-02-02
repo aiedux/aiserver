67bc48d8f0795eed078eee47cb1704f8b3b3fb15
stefian <v_ianta@yahoo.com>
2020-02-02 10:29:00 -0400

    added get function and favicon

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', function (req, res) {
+  res.send('Welcome!');
+});
+
 app.listen(3000);
 console.log('Server Started On Port 3000...');
 
