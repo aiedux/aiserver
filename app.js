761aaf410ad035d3e20bde067b01dd53487df859
stefian <v_ianta@yahoo.com>
2020-02-02 10:20:01 -0400

    added listen port and export app

--- app.js
+++ app.js
@@ -15,3 +15,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
+console.log('Server Started On Port 3000...');
+
+module.exports = app;
